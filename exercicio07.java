9dcb72c528668673459420818d884af8821d05a0
Leandro Barboza <leandro.barboza8@etec.sp.gov.br>
2015-09-12 21:08:45 -0300

    programas java

---
+++ exercicio07.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class exercicio07{
+   public static void main(String[] args){
+   
+   int numero;
+   numero=5;
+   
+      while((numero >0) && (numero <=495))
+         {
+            numero=numero+5;
+            JOptionPane.showMessageDialog(null,numero);
+         }
+         JOptionPane.showMessageDialog(null,"Fim do intervalo");
+         
+      }
+      
+   }
